83967166f08231bffc3952b1ed6721e19388abe9
Mayur Rajendran <mayur.rajendran137@gmail.com>
2018-03-10 09:54:37 -0600

    Updated responsive VR to use color of text. Added plane floor in VR mode Title bar is new trigger to enter VR.

--- init.js
+++ init.js
@@ -1,24 +1,56 @@
 let twoDimensional;
 let baseScene = document.querySelector('a-scene');
 
-function replace(scene) {
-  const replacableDiv = document.getElementById('replace');
-  const text = document.createElement('a-text');
-  text.setAttribute('value', replacableDiv.textContent);
-  text.setAttribute('color', '#4CC3D9');
-  text.setAttribute('position', {x: -1, y: 0.5, z: -3});
+const rgb2Hex = (rgb) => {
+  if (rgb.search("rgb") === -1) return rgb;
+  rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
+  const hex = (x) => ("0" + parseInt(x).toString(16)).slice(-2);
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+};
 
-  replacableDiv.parentNode.removeChild(replacableDiv);
-  scene.appendChild(text);
+function replace(scene) {
+  initializeScene(scene);
+  replaceHeader(scene);
+}
+
+const initializeScene = (scene) => {
+  const floor = document.createElement('a-plane');
+  floor.setAttribute('position', {x: 0, y: 0, z: 0});
+  floor.setAttribute('rotation', {x: -90, y: 0, z: 0});
+  floor.setAttribute('height', 10);
+  floor.setAttribute('width', 10);
+  floor.setAttribute('color', '#fff');
+  scene.appendChild(floor);
+}
+
+const replaceHeader = (scene) => {
+  // Get element to replace
+  const headerElement = document.getElementsByClassName('title-bar')[0];
+  // Create VR object
+  const headerText = document.createElement('a-text');
+  // Set properties
+  headerText.setAttribute('value', headerElement.textContent);
+  console.log(rgb2Hex(getComputedStyle(headerElement).color));
+  headerText.setAttribute('color', rgb2Hex(getComputedStyle(headerElement).color));
+  headerText.setAttribute('position', {x: -1, y: 3, z: -3});
+  // Clean up element from HTML, add to VR
+  headerElement.parentNode.removeChild(headerElement);
+  scene.appendChild(headerText);
 }
 
 function init () {
   const scene = document.querySelector('a-scene');
+  scene.classList.remove('hide');
+  scene.addFullScreenStyles();
+  const camera = document.querySelector('[camera]');
+  camera.setAttribute('position', {x: 0, y: 1.6, z: 0});
   replace(scene);
 }
 
 function resetScene () {
   const scene = document.querySelector('a-scene');
+
+  scene.classList.add('hide');
   scene.addEventListener('enter-vr', function () {
     twoDimensional = document.body.innerHTML;
     init();
@@ -27,6 +59,10 @@ function resetScene () {
   scene.addEventListener('exit-vr', function () {
     document.body.innerHTML = twoDimensional;
     resetScene();
+  });
+
+  document.getElementsByClassName('title-bar')[0].addEventListener('click', () => {
+    scene.enterVR();
   })
 }
 
@@ -38,4 +74,10 @@ baseScene.addEventListener('enter-vr', function () {
 baseScene.addEventListener('exit-vr', function () {
   document.body.innerHTML = twoDimensional;
   resetScene();
+})
+
+baseScene.removeFullScreenStyles();
+
+document.getElementsByClassName('title-bar')[0].addEventListener('click', () => {
+  baseScene.enterVR();
 })
